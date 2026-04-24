ae744a42d9eac10c76ff4d9986ac20c6ead277d2
bart-sysf <bart@sysf.nl>
2026-04-24 17:53:12 +0200

    fix: add middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'static',
-  integrations: [vue()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    output: 'static',
+    integrations: [vue()],
+    adapter: node({
+        mode: 'standalone',
+    }),
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
